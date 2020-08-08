87a1de6791543213294da2215ed13b7b0498156f
Matthew R Becker <becker.mr@gmail.com>
2020-08-08 15:18:00 -0500

    use current dir

--- recipe/zlib-bld.bat
+++ recipe/zlib-bld.bat
@@ -7,7 +7,7 @@ cmake -G "NMake Makefiles" ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -D CMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
-      %SRC_DIR%
+      .
 if errorlevel 1 exit 1
 
 :: Build.
