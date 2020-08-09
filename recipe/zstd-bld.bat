f0262f11bcdc87c347646891fde8ac7e0aa480a0
Matthew R Becker <becker.mr@gmail.com>
2020-08-08 22:00:59 -0500

    try again

--- recipe/zstd-bld.bat
+++ recipe/zstd-bld.bat
@@ -1,10 +1,11 @@
-pushd .\build\cmake
+pushd build\cmake
 cmake -GNinja ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -DCMAKE_INSTALL_LIBDIR="lib" ^
     -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-    -DZSTD_BUILD_SHARED=ON
+    -DZSTD_BUILD_SHARED=ON ^
+    .
 if errorlevel 1 exit 1
 cmake --build . --target install
 if errorlevel 1 exit 1
@@ -16,3 +17,5 @@ copy %PREFIX%\Library\bin\zstd.dll %PREFIX%\Library\bin\libzstd.dll
 if errorlevel 1 exit 1
 copy %PREFIX%\Library\lib\zstd.lib %PREFIX%\Library\lib\libzstd.lib
 if errorlevel 1 exit 1
+
+popd
